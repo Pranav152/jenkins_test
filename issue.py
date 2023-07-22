24868c94fbf349b19f24323e1c785716c4718283
Pranav152 <saipranav152@gmail.com>
2023-07-22 16:12:16 -0700

    Fixed issues

--- issue.py
+++ issue.py
@@ -1,24 +1,24 @@
-def factorial(n):
-    if n == 0:
+def factorial(num_1):
+    if num_1 == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return num_1 * factorial(num_1-1)
 
 def print_message(message):
     print(message)  # Fix the missing indentation for the print statement
 
-def add_numbers(a, b):
-    result = a + b
+def add_numbers(num_1, num_2):
+    result = num_1 + num_2
     return result
 
-number = 5
-factorial_result = factorial(number)
-print("Factorial of", number, "is", factorial_result)
+NUMBER = 5
+factorial_result = factorial(NUMBER)
+print("Factorial of", NUMBER, "is", factorial_result)
 
 print_message("Hello, World!")
 
-result = add_numbers(3, 7)
-print("Addition result:", result)
+RESULT = add_numbers(3, 7)
+print("Addition RESULT:", RESULT)
 
 # Add a blank line after function definition
 def power(base, exponent):
