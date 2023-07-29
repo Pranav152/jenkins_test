c9a80b8836667beb0a5614fe7841588390b01772
Pranav152 <saipranav152@gmail.com>
2023-07-29 11:59:44 -0700

    linted test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
+"""Script to test the add function."""
+
 import unittest
 from main import add
 
-class TestAddFunction(unittest.TestCase):
+# This is a class to test the add function
+class TestAddFunctionTestCase(unittest.TestCase):
+    """Test cases for the add function."""
     def test_add(self):
+        """Test the add function with two numbers."""
         self.assertEqual(add(2, 3), 5)
